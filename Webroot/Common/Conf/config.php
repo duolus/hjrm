b337a18385feaed293fd8e41fb4bb717c08b9ea3
duolus <dungmanhou@gmail.com>
2014-09-10 11:42:30 +0800

    新增

--- Webroot/Common/Conf/config.php
+++ Webroot/Common/Conf/config.php
@@ -1,4 +1,8 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+
+		'LOAD_EXT_CONFIG' => 'db',  // 加载扩展配置文件
+		'ACTION_SUFFIX'   =>  'Action', // 操作方法后缀
+		'URL_HTML_SUFFIX' =>'shtml', //URL伪静态
 );
--- Webroot/Home/Controller/IndexController.class.php
+++ Webroot/Home/Controller/IndexController.class.php
@@ -1,8 +1,9 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Think\Auth;
 class IndexController extends Controller {
-    public function index(){
+    public function indexAction(){
     	 //print_r($_SERVER);
 		// echo "<b  >HTTP_USER_AGENT :</b> ",$_SERVER['HTTP_USER_AGENT'] ;
 		 $start = stripos($_SERVER['HTTP_USER_AGENT'], '(');
@@ -10,17 +11,31 @@ class IndexController extends Controller {
 		 $str2 = substr($_SERVER['HTTP_USER_AGENT'],$start,$end);
 		 $end = stripos($str2, ')');
 		 $str2 = substr($_SERVER['HTTP_USER_AGENT'],$start+1,$end-1);
-		  //echo  $str2;
+		  echo  $str2;
 		// print_r(explode(';', $str2));
 		 if(strpos($str2,"Windows NT") !== false )
 		 {
 		 	echo "正在使用Windows PC设备";
+
 		 }else if(strpos($str2,"Android") !== false )
 		 {
 		 	echo "正在使用安卓设备";
+
 		 }else if(strpos($str2,"Windows Phone") !== false )
 		 {
 		 	echo "正在使用Windows Phone设备";
+		 }
+
+		 //
+		 $auth = new Auth();
+		 print_r($auth->getGroups(1)) ;
+
+		 if($auth->check('test','1'))
+		 {
+		 	echo "通过";
+		 }else
+		 {
+		 	echo "不通过";
 		 }
     }
 }
